ebcbe9bdfce4a38c185a5ba940cfd970dfab4c3d
YOSHIKI <belltree.smile@gmail.com>
2018-06-14 15:50:35 +0900

    I added files.

---
+++ JavaScript/2018.06.14_modal.js
@@ -0,0 +1,8 @@
+(function(){
+'use strict';
+
+var open = document.getElementById('open');
+var close = document.getElementById('close');
+var modal = document.getElementById('modal');
+var mask = document.getElementById('mask');
+})();
